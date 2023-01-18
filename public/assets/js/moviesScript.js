59900ac7dc2a0b5bba8be099367e94476bab00d0
sebastien <heligon.seb@gmail.com>
2023-01-18 11:44:11 +0100

    composer update / creat asset folder in public and add js inside / add boostrap icons / add meta viewport

---
+++ public/assets/js/moviesScript.js
@@ -0,0 +1,50 @@
+let collection, buttonAdd, span;
+
+window.onload = () => {
+    collection = document.querySelector("#movies");
+    span = collection.querySelector("span");
+
+    buttonAdd = document.createElement("button");
+    buttonAdd.className = "add-pictures btn btn-primary";
+    buttonAdd.innerText = "Ajouter une vidéo";
+
+    let newButton = span.append(buttonAdd);
+
+    collection.dataset.index = collection.querySelectorAll('input').length;
+
+    buttonAdd.addEventListener("click", function() {
+        addButton(collection, buttonAdd);
+    });
+}
+
+
+function addButton(collection, newButton) {
+    let prototype = collection.dataset.prototype;
+
+    let index = collection.dataset.index;
+
+    prototype = prototype.replace(/__name__/g, index);
+
+    let content = document.createElement("html");
+    content.innerHTML = prototype;
+
+    let newForm = content.querySelector("div");
+
+    let buttonDelete = document.createElement("button");
+    buttonDelete.type = "button";
+    buttonDelete.className = "btn btn-danger";
+    buttonDelete.id = "delete-picture-" + index;
+    buttonDelete.innerText = "Supprimer cette image";
+
+    newForm.append(buttonDelete);
+
+    collection.dataset.index++;
+
+    let buttonAdd = collection.querySelector(".add-pictures");
+    span.insertBefore(newForm, buttonAdd);
+
+    buttonDelete.addEventListener('click', function() {
+        this.previousElementSibling.parentElement.remove();
+    });
+}
+
