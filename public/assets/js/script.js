39deb833ca2ade2ad2cb9ae80d759ec1c2c78434
sebastien <heligon.seb@gmail.com>
2023-03-08 20:18:58 +0100

    in progress CSS

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,21 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let elPictures = document.getElementById('trick_form_pictures');
-
-    let picturesInput = elPictures.getElementsByTagName('input');
-
-    for (let i = 0; i < picturesInput.length; i++) {
-        picturesInput[i].required = false;
+    if (window.location.pathname === "/tricks/add") {
+        let elPictures = document.getElementById('trick_form_pictures');
+    
+        let picturesInput = elPictures.getElementsByTagName('input');
+    
+        for (let i = 0; i < picturesInput.length; i++) {
+            picturesInput[i].required = false;
+        }
+    
+    
+        let elMovies = document.getElementById('trick_form_movies');
+    
+        let moviesInput = elMovies.getElementsByTagName('input');
+    
+        for (let i = 0; i < moviesInput.length; i++) {
+            moviesInput[i].required = false;
+        }
+    
+        elPictures.classList.add('d-none');
+        elMovies.classList.add('d-none');
     }
 
-
-    let elMovies = document.getElementById('trick_form_movies');
-
-    let moviesInput = elMovies.getElementsByTagName('input');
-
-    for (let i = 0; i < moviesInput.length; i++) {
-        moviesInput[i].required = false;
+    function containsNumbers(str) {
+        return /\d/.test(str);
     }
 
-    elPictures.classList.add('d-none');
-    elMovies.classList.add('d-none');
+    if (containsNumbers(window.location.pathname)) {
+        console.log('tricks');
+        /**
+         * Toggle media section in trick page on click button
+         */
+        document.querySelector('.see-more').addEventListener('click', function() {
+            let divMedia = document.querySelector('.div-media-collapse');
+    
+            console.log('click click');
+            divMedia.toggleAttribute('d-none');
+        });
+
+    }
+
 });
