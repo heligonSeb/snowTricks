7c216ac14a8db662a42b789b77a2b5c63c8a0ae3
sebastien <heligon.seb@gmail.com>
2023-04-02 16:26:17 +0200

    upgrade score SymfonyInsight

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,7 +6,6 @@ use App\Repository\FigureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
